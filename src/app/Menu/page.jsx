26796e7b830a0c99eb80682094a908cb79a42afa
Muhammad Taha Raees <raeesmuhammadtaha@gmail.com>
2023-10-30 23:17:52 +0100

    Your commit message here

--- src/app/Menu/page.jsx
+++ src/app/Menu/page.jsx
@@ -1,38 +1,36 @@
-import "./Menu.scss"
+import "./Menu.scss";
 import Image from "next/image";
 import Link from "next/link";
 
-const getData= async ()=>{
-  const apiUrl = process.browser ? `${window.location.origin}/api/categories` : "http://localhost:3000/api/categories";
+const getData = async () => {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL + "/api/categories";
+  
+  const res = await fetch(apiUrl, {
+    cache: "no-store"
+  });
 
-
-
-
-const res = await fetch(apiUrl, {
-  cache: "no-store",
-});
-  if(!res.ok){
-    throw new Error("Failed")
+  if (!res.ok) {
+    throw new Error("Failed");
   }
-  return res.json()
+  
+  return res.json();
 }
 
 const Menu = async () => {
-  const menu = await getData()
+  const menu = await getData();
   return (
     <div className="Category">
       <h1>Category</h1>
       <div className="menu-items">
-      {menu.map((category) => (
-       <div className="item" key={category.id} style={{ backgroundImage: `url(${category.img})`, backgroundSize: 'cover' }}>
-        <Link href={'/Menu/${category.slug}'}>
-        <h1>{category.title}</h1>
-        <p>{category.desc}</p>
-        <button>Explore</button>
-       </Link>
-       </div>
-))}
-
+        {menu.map((category) => (
+          <div className="item" key={category.id} style={{ backgroundImage: `url(${category.img})`, backgroundSize: 'cover' }}>
+            <Link href={`/Menu/${category.slug}`}>
+              <h1>{category.title}</h1>
+              <p>{category.desc}</p>
+              <button>Explore</button>
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
